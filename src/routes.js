74370573d123ae17905278209052f023523f85a8
Rodrigo Sobral <rodrigo.sobral@usabit.com.br>
2022-09-08 14:15:53 -0300

    adicionando projeto existente

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import Carrinho from "pages/Carrinho";
+import Feira from "pages/Feira";
+import Login from "pages/Login";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { UsuarioProvider } from "common/context/Usuario";
+
+const Router = () => {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<UsuarioProvider>
+					<Route exact path="/">
+						<Login />
+					</Route>
+					<Route path="/feira">
+						<Feira />
+					</Route>
+				</UsuarioProvider>
+				<Route path="/carrinho">
+					<Carrinho />
+				</Route>
+			</Switch>
+		</BrowserRouter>
+	);
+};
+
+export default Router;
